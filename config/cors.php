54bc789ba8470dceb1f7befd383cabcfac529ebe
TRAORE LACINA <149535272+Traorelacina@users.noreply.github.com>
2025-12-02 13:22:50 +0000

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,9 @@ return [
         'sanctum/csrf-cookie',
         'login',
         'logout',
+        'track-view',
+        'track-pixel',
+        'health',
     ],
 
     'allowed_methods' => ['*'],
@@ -13,12 +16,22 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://localhost:5175',
-        'https://gloryevent.netlify.app',
         'http://127.0.0.1:5175',
-        "http://localhost:5173",
+        'http://localhost:5173',
+        'https://gloryevent.netlify.app',
+        'https://*.netlify.app', // AJOUTER CETTE LIGNE
+    ],
+
+    'allowed_origins_patterns' => [
+        '/^https?:\/\/.*\.netlify\.app$/',
+        '/^https?:\/\/localhost:\d+$/',
     ],
 
     'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
 
     'supports_credentials' => true,
 ];
